b4d0e9414ca3677f3c73bd16c876ed1e499e2f78
Brix-Julianda <brix.julianda1702@gmail.com>
2025-02-20 12:37:11 +0800

    Created 2 Forms on Create Policy (with Transition) from next button

--- resources/views/create-account.blade.php
+++ resources/views/create-account.blade.php
@@ -3,6 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Add Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <style>
         body {
             font-family: 'Inter', sans-serif;
@@ -65,10 +66,9 @@
         .create-account-2 {
 
             height: 847px;
-            width: 710px;
+            width: 830px;
             margin: auto;
             display: none;
-            margin-left: 700px;
         }
 
         .question {
@@ -78,19 +78,31 @@
         }
     </style>
     <script>
-                $(document).ready(function() {
-                    $('#primary-button-1').click(function(){
-                        $("#container-form-1").hide();
-                        $("#container-form-2").show();
-                    })
-                    
-                });
-            </script>
+        $(document).ready(function() {
+
+            $('#form-2').hide();
+
+            $('#next-btn-1').click(function() {
+                $("#form-1").hide();
+                $("#form-2").show();
+            });
+
+            $("#cancel-btn-2").click(function(e) {
+                e.preventDefault();
+                $("#form-2").hide();
+                $("#form-1").show();
+            })
+
+
+
+
+        });
+    </script>
 </head>
 
 <body>
 
-    <div class="create-account-1" id="container-form-1">
+    <div class="create-account-1">
         <div class="container-fluid">
             <div class="row">
                 <!-- Stepper Component (Sticky on Left) -->
@@ -100,7 +112,7 @@
 
 
 
-                <div class="form-container">
+                <div class="form-container" id="form-1">
 
                     <div class="container py-4" style="margin-top:35px;">
                         <x-createaccount.back-button title="Create account as a Policyholder" backUrl="{{ url()->previous() }}" />
@@ -222,155 +234,147 @@
 
 
                 </div>
-            </div>
 
-            <div class="create-account-2" id="container-form-2">
+                <div class="create-account-2" id="form-2">
 
-                <div class="container py-4" style="margin-top:35px;">
-                    <x-createaccount.back-button title="Create account as a Policyholder" backUrl="{{ url()->previous() }}" />
-                </div>
-
-                <div class="container-form-2">
-                    <div class="form-title" style="margin-left:35px;">
-                        <x-CreateAccount.form-title title="Getting to know you" />
+                    <div class="container py-4" style="margin-top:35px;">
+                        <x-createaccount.back-button title="Create account as a Policyholder" backUrl="{{ url()->previous() }}" />
                     </div>
 
-                    <p class="question">What policy are you interested in?<span class="asterisk-policy">*</span>(you may select as many as you want)</p>
-
-                    <div class="row">
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Excel-Plus"
-                                label="Auto Excel Plus"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Furtect"
-                                label="Furtect"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Condo-Excel"
-                                label="Condo Excel"
-                                checked="false" />
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Travel-Plus"
-                                label="International Travel Plus"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Bonds"
-                                label="Bonds"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Dtravel-Plus"
-                                label="Domestic Travel Plus"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="Hack-Guard"
-                                label="Hack Guard"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <p class="question">Do you want to be contacted by a Cocogen Representative? <span class="asterisk-policy">*</span></p>
-
-                        <div class="col">
-                            <x-Buttons.pill-button
-                                id="contacted-pill"
-                                :pillOneText="'No, I will explore Cocogen products myself'"
-                                :pillTwoText="'Yes, I need a representative to talk to me'" />
-                        </div>
-                    </div>
-
-                    <p class="question">Which Cocogen branch should you wish to be contacted by? <span class="asterisk-policy">*</span></p>
-
-                    <div class="row d-flex align-items-center">
-                        <div class="col-md-6">
-                            <x-fields.dropdown-field
-                                name="Branch"
-                                label="Select (1) Cocogen branch"
-                                :options="['Pasig Branch' => 'Pasig Branch', 'Makati Branch', 'Quezon Branch']"
-                                width="345px"
-                                height="56px"
-                                required />
+                    <div class="container-form-2">
+                        <div class="form-title" style="margin-left:35px;">
+                            <x-CreateAccount.form-title title="Getting to know you" />
                         </div>
 
-                        <div class="col-md-6">
-                            <x-fields.dropdown-field
-                                name="Availability"
-                                label="Hours you are available to be contacted"
-                                :options="['Morning' => 'Morning', 'Afternoon', 'Evening']"
-                                width="345px"
-                                height="56px"
-                                required />
+                        <p class="question">What policy are you interested in?<span class="asterisk-policy">*</span>(you may select as many as you want)</p>
+
+                        <div class="row">
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Excel-Plus"
+                                    label="Auto Excel Plus"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Furtect"
+                                    label="Furtect"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Condo-Excel"
+                                    label="Condo Excel"
+                                    checked="false" />
+                            </div>
                         </div>
-                    </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Travel-Plus"
+                                    label="International Travel Plus"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Bonds"
+                                    label="Bonds"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Dtravel-Plus"
+                                    label="Domestic Travel Plus"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="Hack-Guard"
+                                    label="Hack Guard"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <p class="question">Do you want to be contacted by a Cocogen Representative? <span class="asterisk-policy">*</span></p>
+
+                            <div class="col">
+                                <x-Buttons.pill-button
+                                    id="contacted-pill"
+                                    :pillOneText="'No, I will explore Cocogen products myself'"
+                                    :pillTwoText="'Yes, I need a representative to talk to me'" />
+                            </div>
+                        </div>
+
+                        <p class="question">Which Cocogen branch should you wish to be contacted by? <span class="asterisk-policy">*</span></p>
+
+                        <div class="row d-flex align-items-center">
+                            <div class="col-md-6">
+                                <x-fields.dropdown-field
+                                    name="Branch"
+                                    label="Select (1) Cocogen branch"
+                                    :options="['Pasig Branch' => 'Pasig Branch', 'Makati Branch', 'Quezon Branch']"
+                                    width="345px"
+                                    height="56px"
+                                    required />
+                            </div>
+                        </div>
 
 
 
-                    <p class="question">How do you want to be contacted? <span class="asterisk-policy">*</span>(You may select more than one)</p>
-                    <div class="row">
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="email"
-                                label="Email"
-                                checked="false" />
-                            <x-buttons.checkbox-button
-                                id="sms"
-                                label="SMS"
-                                checked="false" />
-                            <x-buttons.checkbox-button
-                                id="call"
-                                label="Call"
-                                checked="false" />
-                        </div>
-                        <div class="col">
-                            <x-buttons.checkbox-button
-                                id="messenger"
-                                label="Messenger"
-                                checked="false" />
-                        </div>
-                    </div>
-
-                    <div class="row" style="margin-top:35px; margin-bottom:35px;">
-                        <div class="col-md-6">
-                            <x-Buttons.secondary-button>Cancel </x-Buttons.secondary-button>
+                        <p class="question">How do you want to be contacted? <span class="asterisk-policy">*</span>(You may select more than one)</p>
+                        <div class="row">
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="email"
+                                    label="Email"
+                                    checked="false" />
+                                <x-buttons.checkbox-button
+                                    id="sms"
+                                    label="SMS"
+                                    checked="false" />
+                                <x-buttons.checkbox-button
+                                    id="call"
+                                    label="Call"
+                                    checked="false" />
+                            </div>
+                            <div class="col">
+                                <x-buttons.checkbox-button
+                                    id="messenger"
+                                    label="Messenger"
+                                    checked="false" />
+                            </div>
                         </div>
 
-                        <div class="col-md-6">
-                            <x-Buttons.primary-button>Next </x-Buttons.primary-button>
-                        </div>
+                        <div class="row" style="margin-top:35px; margin-bottom:35px;">
+                            <div class="col-md-6">
+                                <x-Buttons.secondary-arrow-button id="cancel-btn-2">Cancel </x-Buttons.secondary-arrow-button>
+                            </div>
 
+                            <div class="col-md-6">
+                                <x-Buttons.primary-arrow-button id="next-btn-2">Next </x-Buttons.primary-arrow-button>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
+
+
 
 
 
             <!-- Add Bootstrap JavaScript (optional) -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-            
+
 </body>
