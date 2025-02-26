f28117acae0f62410b6ac15bff451fb815ae3ce0
Brix-Julianda <brix.julianda1702@gmail.com>
2025-02-26 14:48:51 +0800

    merge

--- resources/views/create-account.blade.php
+++ resources/views/create-account.blade.php
@@ -1,82 +1,21 @@
 <head>
-    <!-- Add Bootstrap CSS -->
+    <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Add Google Fonts -->
+    <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
     <style>
-        html,
-        body {
-            margin: 0px;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            font-family: 'Inter', sans-serif;
-            display: flex;
-            flex-direction: row;
-            box-sizing: border-box;
-        }
 
-        .stepper {
-            position: absolute;
-            width: auto;
-            top: 0;
-            left: 0;
-            display: none;
-            height: 100%;
-        }
-
-        #step1 {
-            display: flex;
-        }
-
-        .step {
-
-            display: flex;
-            
-        }
     </style>
-
-
-    <script>
-        
-    </script>
-
 </head>
 
 <body>
-
     
-
-        <form method="" action="" id="StepForm">
-        <div class="step" id="step1">@include('Register.failed-otp-page')</div>
-        
-
-        </form>
-
-
-        
-<!--         
-        Register.create-account-1
-        Register.create-account-2
-        Register.create-account-2-2
-
-        Register.your-identity-1
-        Register.your-identity-2
-        Register.your-identity-3
-
-        Register.otp-page
-        Register.failed-otp-page
-        Register.account-registered
-         -->
-
-        
-        
+    @include('Register.')
     
 
 
-    <!-- Add Bootstrap JavaScript (optional) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
+
